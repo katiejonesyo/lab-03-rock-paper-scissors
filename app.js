c87a9908b90676a0a94b7f69002f9218ac50efa8
Katelinn Jones <katiejonesyo@gmail.com>
2020-09-30 20:44:10 -0700

    example test

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// import functions and grab DOM elements
 import { getRandomThrow, userWins } from './get-random-throw.js';
 
 const playButton = document.querySelector('playButton');
@@ -13,3 +12,29 @@ let losses = 0;
 let draws = 0; 
 
 
+playButton.addEventListener('click', () => {
+   
+    const randomGuess = getRandomThrow();
+  
+    const checkedRadioButton = document.querySelector(':checked');
+    const userGuess = checkedRadioButton.value;
+
+    let total = wins + losses;
+
+    if (userGuess === randomGuess) {
+        draws++;
+        drawSpan.textContent = draws;
+       
+    } else if (userWins(userGuess, randomGuess) === true) {
+        wins++;
+      
+    } else if (userWins(userGuess, randomGuess) === false) {
+        losses++;
+        lossSpan.textContent = losses;
+       
+    } 
+});
+
+resetButton.addEventListener('click', () => {
+
+});
--- test/example.test.js
+++ test/example.test.js
@@ -1,17 +1,9 @@
-// import { example } from '../example.js';
-
+import { getRandomThrow, userWins } from '../get-random-throw.js';
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
-    const expected = true;
-    
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const actual = false;
 
-    //Expect
-    // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+test('test should return true if a string is generated.', (expect) => {
+    const actual = getRandomThrow();
+    expect.equal(typeof actual, 'string');
 });
+
