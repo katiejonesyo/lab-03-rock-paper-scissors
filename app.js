0979719e17219626e3004df3e34423b1bd10a039
ErikGraciosa <erikgraciosa@gmail.com>
2020-10-01 15:00:05 -0700

    updated userWins, updated reset button, updated querySelector syntax

--- app.js
+++ app.js
@@ -1,40 +1,42 @@
 import { getRandomThrow, userWins } from './get-random-throw.js';
 
-const playButton = document.querySelector('playButton');
-const winnerSpan = document.querySelector('wins');
-const lossSpan = document.querySelector('losses');
-const drawSpan = document.querySelector('draws');
-
-const resetButton = document.querySelector('resetButton');
+const playButton = document.querySelector('#playButton');
+const winnerSpan = document.querySelector('#winnerSpan');
+const lossSpan = document.querySelector('#lossesSpan');
+const drawSpan = document.querySelector('#drawSpan');
+const resetButton = document.querySelector('#resetButton');
 
 let wins = 0;
 let losses = 0;
 let draws = 0; 
 
 
-playButton.addEventListener('click', () => {
-   
-    const randomGuess = getRandomThrow();
-  
+playButton.addEventListener('click', () => {   
+    const randomGuess = getRandomThrow();  
     const checkedRadioButton = document.querySelector(':checked');
     const userGuess = checkedRadioButton.value;
 
-    let total = wins + losses;
-
     if (userGuess === randomGuess) {
         draws++;
-        drawSpan.textContent = draws;
-       
-    } else if (userWins(userGuess, randomGuess) === true) {
-        wins++;
-      
-    } else if (userWins(userGuess, randomGuess) === false) {
+        drawSpan.textContent = draws;       
+    } else if (userWins(userGuess, randomGuess) === 'win') {
+        wins++;        
+        winnerSpan.textContent = wins;
+
+    } else if (userWins(userGuess, randomGuess) === 'lose') {
         losses++;
         lossSpan.textContent = losses;
-       
     } 
 });
 
-resetButton.addEventListener('click', () => {
 
+resetButton.addEventListener('click', () => {
+    //Reset variable states
+    wins = 0;
+    losses = 0;
+    draws = 0; 
+    //Write back varibles to spans
+    drawSpan.textContent = draws;
+    winnerSpan.textContent = wins;
+    lossSpan.textContent = losses;
 });
